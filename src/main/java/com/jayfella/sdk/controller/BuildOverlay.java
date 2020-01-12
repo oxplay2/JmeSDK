5c757d601772345d3fc477e20cff1617247c704f
jayfella <jayfella@gmail.com>
2020-01-12 19:41:37 +0000

    Add getters for progressindicator and rebuild button.

--- src/main/java/com/jayfella/sdk/controller/BuildOverlay.java
+++ src/main/java/com/jayfella/sdk/controller/BuildOverlay.java
@@ -3,7 +3,9 @@ package com.jayfella.sdk.controller;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressIndicator;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -11,6 +13,8 @@ import java.util.ResourceBundle;
 public class BuildOverlay implements Initializable {
 
     @FXML private Label statusLabel;
+    @FXML private ProgressIndicator progressIndicator;
+    @FXML private Button rebuildButton;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -20,5 +24,14 @@ public class BuildOverlay implements Initializable {
     public void bind(ObservableValue<String> observableValue) {
         statusLabel.textProperty().bind(observableValue);
     }
+
+    public ProgressIndicator getProgressIndicator() {
+        return progressIndicator;
+    }
+
+    public Button getRebuildButton() {
+        return rebuildButton;
+    }
+
 
 }
